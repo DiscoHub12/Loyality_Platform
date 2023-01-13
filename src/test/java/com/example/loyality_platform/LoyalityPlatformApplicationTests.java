5a0079086c8c5bddc774495f54f7904182d8f969
AlexDysco12 <96992944+DiscoHub12@users.noreply.github.com>
2023-01-13 16:58:35 +0100

    [INIT] Spring-Boot Project.

---
+++ src/test/java/com/example/loyality_platform/LoyalityPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loyality_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoyalityPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
