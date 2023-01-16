8f2af7c45e474d3857862474cd5cb07d8b24c281
Sofia <scattolinisofia26@gmail.com>
2023-01-16 21:56:21 +0100

    [UPDATE] GestorePuntoVendita.java

--- src/main/java/loyality_platform_model/Models/GestorePuntoVendita.java
+++ src/main/java/loyality_platform_model/Models/GestorePuntoVendita.java
@@ -1,4 +1,46 @@
 package loyality_platform_model.Models;
 
 public class GestorePuntoVendita {
+
+    private static int id;
+
+    private final String nome, cognome;
+
+    private String email;
+
+    public GestorePuntoVendita(String nome, String cognome, String email) {
+        id++;
+        this.nome = nome;
+        this.cognome = cognome;
+        this.setEmail(email);
+    }
+
+    public static int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "GestorePuntoVendita{" +
+                "nome='" + nome + '\'' +
+                ", cognome='" + cognome + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
