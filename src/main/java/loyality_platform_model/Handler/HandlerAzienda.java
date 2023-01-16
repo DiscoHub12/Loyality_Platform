538e0b80b84974f005a51f8c4af41cb1f238e5d9
Sofia <scattolinisofia26@gmail.com>
2023-01-16 21:47:35 +0100

    [UPDATE] HandlerAzienda.java

--- src/main/java/loyality_platform_model/Handler/HandlerAzienda.java
+++ src/main/java/loyality_platform_model/Handler/HandlerAzienda.java
@@ -1,4 +1,46 @@
 package loyality_platform_model.Handler;
 
+import loyality_platform_model.DBController.DBMSController;
+import loyality_platform_model.Models.Azienda;
+import loyality_platform_model.Models.Dipendente;
+import loyality_platform_model.Models.ProgrammaFedelta;
+
+import java.util.Objects;
+import java.util.Set;
+
 public class HandlerAzienda {
+
+    private final DBMSController dbmsController;
+
+    private final Azienda azienda;
+
+    public HandlerAzienda(Azienda azienda) {
+        Objects.requireNonNull(azienda);
+        this.dbmsController = DBMSController.getInstance();
+        this.azienda = azienda;
+    }
+
+    public String getSpazioFedelta(){
+       return this.getAzienda().getSpazioFedelta().toString();
+    }
+
+    public Set<Dipendente> getUtenti() {
+        return this.getAzienda().getDipendenti();
+    }
+
+    public String getDetailsUtente(Dipendente dipendente){
+        Objects.requireNonNull(dipendente);
+        if(this.getAzienda().getDipendenti().contains(dipendente))
+            return dipendente.getDetails();
+        return "Dipendente errato";
+    }
+
+    public Set<ProgrammaFedelta> getProgrammiAttivi(){
+        return this.getAzienda().getProgrammiAttivi();
+    }
+
+
+    public Azienda getAzienda(){
+        return azienda;
+    }
 }
