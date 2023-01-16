688b1aa66f5ba1fb377b91428401d4202f79afe0
Sofia <scattolinisofia26@gmail.com>
2023-01-16 21:53:04 +0100

    [UPDATE] GestorePiattaforma.java

--- src/main/java/loyality_platform_model/Models/GestorePiattaforma.java
+++ src/main/java/loyality_platform_model/Models/GestorePiattaforma.java
@@ -1,4 +1,45 @@
 package loyality_platform_model.Models;
 
 public class GestorePiattaforma {
+
+    private static int id;
+
+    private final String nome, cognome;
+
+    private String email;
+
+    public GestorePiattaforma(String nome, String cognome) {
+        this.nome = nome;
+        this.cognome = cognome;
+    }
+
+    public static int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    @Override
+    public String toString() {
+        return "GestorePiattaforma{" +
+                "nome='" + nome + '\'' +
+                ", cognome='" + cognome + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
