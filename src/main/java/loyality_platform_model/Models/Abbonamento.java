4ad967eb6b506ac212d6620dee4184e0ad7d3f18
Sofia <scattolinisofia26@gmail.com>
2023-01-16 17:06:19 +0100

    [IMPLEMENT] Abbonamento.java

--- src/main/java/loyality_platform_model/Models/Abbonamento.java
+++ src/main/java/loyality_platform_model/Models/Abbonamento.java
@@ -1,4 +1,81 @@
 package loyality_platform_model.Models;
 
+import java.util.Date;
+
+/**
+ * Class that represents a subscription that can be
+ * purchased by companies that want to join the platform.
+ */
 public class Abbonamento {
+    private static int id;
+
+    private final Date dataInizio;
+
+    private Date dataFine;
+
+    private double prezzo;
+
+    public Abbonamento(Date dataInizio, Date dataFine, double prezzo) {
+        id++;
+        this.dataInizio = dataInizio;
+        this.dataFine = dataFine;
+        this.setPrezzo(prezzo);
+    }
+
+    /**
+     * This method returns the subscription id.
+     * @return the subscription id.
+     */
+    public static int getId() {
+        return id;
+    }
+
+    /**
+     * This method returns me the start date of the subscription
+     * @return the start date of the subscription
+     */
+    public Date getDataInizio() {
+        return this.dataInizio;
+    }
+
+    /**
+     * This method returns me the end date of the subscription
+     * @return the end date of the subscription
+     */
+    public Date getDataFine() {
+        return this.dataFine;
+    }
+
+    /**
+     * This method returns me the subscription price.
+     * @return the subscription price.
+     */
+    public double getPrezzo() {
+        return this.prezzo;
+    }
+
+    /**
+     * This method sets the subscription price to the one passed as a parameter
+     * @param prezzo new price
+     */
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    /**
+     * This method renews my subscription end date.
+     * @param dataFine new end date
+     */
+    public void rinnovaAbbonamento(Date dataFine){
+        this.dataFine = dataFine;
+    }
+
+    @Override
+    public String toString() {
+        return "Abbonamento{" +
+                "dataInizio=" + dataInizio +
+                ", dataFine=" + dataFine +
+                ", prezzo=" + prezzo +
+                '}';
+    }
 }
