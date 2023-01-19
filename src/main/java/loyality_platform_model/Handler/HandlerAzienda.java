f1cdfd556636878573b543f75efc15a5ab45f677
Sofia <scattolinisofia26@gmail.com>
2023-01-19 11:28:32 +0100

    Apportate modifiche a HandlerAzienda.java

--- src/main/java/loyality_platform_model/Handler/HandlerAzienda.java
+++ src/main/java/loyality_platform_model/Handler/HandlerAzienda.java
@@ -2,6 +2,7 @@ package loyality_platform_model.Handler;
 
 import loyality_platform_model.DBController.DBMSController;
 import loyality_platform_model.Models.Azienda;
+import loyality_platform_model.Models.Cliente;
 import loyality_platform_model.Models.Dipendente;
 import loyality_platform_model.Models.ProgrammaFedelta;
 
@@ -14,10 +15,19 @@ public class HandlerAzienda {
 
     private final Azienda azienda;
 
+    private static HandlerAzienda instance;
+
     public HandlerAzienda(Azienda azienda) {
         Objects.requireNonNull(azienda);
         this.dbmsController = DBMSController.getInstance();
         this.azienda = azienda;
+    }
+
+    public static HandlerAzienda getInstance() {
+        if(instance == null){
+            instance = new HandlerAzienda(getInstance().azienda);
+        }
+        return instance;
     }
 
     public String getSpazioFedelta(){
@@ -32,15 +42,38 @@ public class HandlerAzienda {
         Objects.requireNonNull(dipendente);
         if(this.getAzienda().getDipendenti().contains(dipendente))
             return dipendente.getDetails();
-        return "Dipendente errato";
+        return "Dipendente non trovato";
     }
 
     public Set<ProgrammaFedelta> getProgrammiAttivi(){
         return this.getAzienda().getProgrammiAttivi();
     }
 
+    public String getDetailsProgrammaFedelta(ProgrammaFedelta programmaFedelta){
+        Objects.requireNonNull(programmaFedelta);
+        if(this.getAzienda().getProgrammiAttivi().contains(programmaFedelta)){
+            return programmaFedelta.toString();
+        }
+        return "Programma fedeltà non trovato";
+    }
+
+    public Set<Cliente> getClienti(){
+        return this.getAzienda().getClienti();
+    }
+
+    public String getDetailsCliente(Cliente cliente){
+        Objects.requireNonNull(cliente);
+        if(this.getAzienda().getClienti().contains(cliente)){
+            return cliente.toString();
+        }
+        return "Cliente non trovato";
+    }
 
     public Azienda getAzienda(){
         return azienda;
+    }
+
+    public String getDetailsAzienda() {
+        return this.getAzienda().toString();
     }
 }
