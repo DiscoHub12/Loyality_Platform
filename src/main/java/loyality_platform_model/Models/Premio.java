892ccecd3d086997453fbedb2d4cab51d61e2cd5
DiscoHub12 <discordo12di@gmail.com>
2023-01-19 10:21:34 +0100

    [ADD] comment into Premio.java class.

--- src/main/java/loyality_platform_model/Models/Premio.java
+++ src/main/java/loyality_platform_model/Models/Premio.java
@@ -1,22 +1,43 @@
 package loyality_platform_model.Models;
 
+/**
+ * Class that rapresents the concept of Award.
+ * A Reward can be redeemed by a particular Costumer (member)
+ * if he reaches the required number of points.
+ */
 public class Premio {
 
+    /**
+     * This attribute rapresents the
+     * unique id of this award
+     */
     private static int idPremio;
 
+    /**
+     * This attribute rapresents the name
+     * of this Award.
+     */
     private String nome;
 
+    /**
+     * Constructor that allow to create an object
+     * of type Premio.
+     * @param nome the name of the Award.
+     */
     public Premio(String nome){
         this.nome = nome;
     }
+
 
     public static int getIdPremio() {
         return idPremio;
     }
 
+
     public static void setIdPremio(int idPremio) {
         Premio.idPremio = idPremio;
     }
+
 
     public String getNome() {
         return nome;
