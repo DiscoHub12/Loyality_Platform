1a9af53c3e798e3619f524ed2b6deb6e736b8abb
DiscoHub12 <discordo12di@gmail.com>
2023-01-19 10:58:53 +0100

    [ADD] Comments and two new Method in DBMSController.java class.

--- src/main/java/loyality_platform_model/DBController/DBMSController.java
+++ src/main/java/loyality_platform_model/DBController/DBMSController.java
@@ -1,20 +1,62 @@
 package loyality_platform_model.DBController;
 
-//Todo finire.
+import loyality_platform_model.Models.Cliente;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * Class representing the DataBase, capable of persisting data within
+ * the Platform.
+ */
 public class DBMSController {
 
+    /**
+     * This attribute represents the instance of the class, capable of
+     * being able to invoke it without instantiating a new class and losing persistence.
+     */
     private static DBMSController instance;
 
+    /**
+     * This attribute rapresent the name of this DataBase.
+     */
     private final String nameDb;
 
-    public DBMSController(String nameDb){
+    /**
+     * This attribute represents the list of (final) Clients
+     * registered within the platform.
+     */
+    private final Set<Cliente> clientiIscritti;
+
+
+    public DBMSController(String nameDb) {
         this.nameDb = nameDb;
+        this.clientiIscritti = new HashSet<>();
     }
 
     public static DBMSController getInstance() {
-        if(instance == null){
+        if (instance == null) {
             instance = new DBMSController(getInstance().nameDb);
         }
         return instance;
+    }
+
+    public Set<Cliente> getClientiIscritti() {
+        return this.clientiIscritti;
+    }
+
+    public void addCliente(Cliente cliente) {
+        Objects.requireNonNull(cliente);
+        if (clientiIscritti.contains(cliente))
+            throw new IllegalArgumentException("Costumer already present.");
+        this.clientiIscritti.add(cliente);
+    }
+
+    public void removeCliente(Cliente cliente) {
+        Objects.requireNonNull(cliente);
+        if (!clientiIscritti.contains(cliente))
+            throw new IllegalArgumentException("Costumer does not exist.");
+        this.clientiIscritti.remove(cliente);
     }
 }
