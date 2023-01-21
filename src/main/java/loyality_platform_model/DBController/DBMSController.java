780708d57d28f50a64b9e83d8770a571a72d0190
Sofia <scattolinisofia26@gmail.com>
2023-01-21 18:29:21 +0100

    [UPDATE] DBMSController.java

--- src/main/java/loyality_platform_model/DBController/DBMSController.java
+++ src/main/java/loyality_platform_model/DBController/DBMSController.java
@@ -1,10 +1,9 @@
 package loyality_platform_model.DBController;
 
+import loyality_platform_model.Models.Azienda;
 import loyality_platform_model.Models.Cliente;
-
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
+import loyality_platform_model.Models.Dipendente;
+import java.util.*;
 
 /**
  * Class representing the DataBase, capable of persisting data within
@@ -29,10 +28,13 @@ public class DBMSController {
      */
     private final Set<Cliente> clientiIscritti;
 
+    private final Map<Azienda, Set<Dipendente>> dipendentiAzienda;
+
 
     public DBMSController(String nameDb) {
         this.nameDb = nameDb;
         this.clientiIscritti = new HashSet<>();
+        this.dipendentiAzienda = new HashMap<>();
     }
 
     public static DBMSController getInstance() {
@@ -58,5 +60,47 @@ public class DBMSController {
         if (!clientiIscritti.contains(cliente))
             throw new IllegalArgumentException("Costumer does not exist.");
         this.clientiIscritti.remove(cliente);
+    }
+
+    public Map<Azienda, Set<Dipendente>> getDipendentiAzienda() {
+        return dipendentiAzienda;
+    }
+
+    public void addDipendenteAzienda(Azienda azienda, Dipendente dipendente){
+        Objects.requireNonNull(azienda);
+        Objects.requireNonNull(dipendente);
+        for(Map.Entry<Azienda, Set<Dipendente>> entry : this.getDipendentiAzienda().entrySet()){
+            if(azienda.equals(entry.getKey())){
+                entry.getValue().add(dipendente);
+            }
+            throw new IllegalArgumentException("Company not exists");
+        }
+    }
+
+    public void modificaDipendenteAzienda(Dipendente olddipendente, Azienda azienda, Dipendente newDipendente){
+        Objects.requireNonNull(azienda);
+        Objects.requireNonNull(olddipendente);
+        Objects.requireNonNull(newDipendente);
+       for(Map.Entry<Azienda, Set<Dipendente>> entry : this.getDipendentiAzienda().entrySet()){
+           if(azienda.equals(entry.getKey())){
+               if(entry.getValue().contains(olddipendente)){
+                   entry.getValue().remove(olddipendente);
+                   entry.getValue().add(newDipendente);
+               }
+               throw new IllegalArgumentException("User not exists");
+           }
+           throw new IllegalArgumentException("Company not exists");
+       }
+    }
+
+    public void removeDipendenteAzienda(Azienda azienda, Dipendente dipendente) {
+        Objects.requireNonNull(azienda);
+        Objects.requireNonNull(dipendente);
+        for (Map.Entry<Azienda, Set<Dipendente>> entry : this.getDipendentiAzienda().entrySet()) {
+            if (azienda.equals(entry.getKey())) {
+                entry.getValue().remove(dipendente);
+            }
+            throw new IllegalArgumentException("Company not exists");
+        }
     }
 }
