1cb35fa2fd5256a808f249ba682eaca0bd904993
Sofia <scattolinisofia26@gmail.com>
2023-01-21 16:45:36 +0100

    Ultima versione 2 iterazione SMS.java

--- src/main/java/loyality_platform_model/Models/SMS.java
+++ src/main/java/loyality_platform_model/Models/SMS.java
@@ -1,5 +1,12 @@
 package loyality_platform_model.Models;
 
+import java.util.Objects;
+
+/**
+ * IMPLEMENTED BY : Sofia Scattolini.
+ *
+ * This class represents an SMS that may or may not have a predefined configuration.
+ */
 public class SMS {
 
     private String testo;
@@ -14,27 +21,54 @@ public class SMS {
         this.setConfigurato(configurato);
     }
 
+    /**
+     * This method returns the text of the SMS.
+     * @return the text of the SMS.
+     */
     public String getTesto() {
         return testo;
     }
 
+    /**
+     * This method sets the text of the SMS.
+     * @param testo the text of the SMS.
+     */
     public void setTesto(String testo) {
+        if (Objects.equals(testo, ""))
+            throw new IllegalArgumentException("Illegal text for sms.");
         this.testo = testo;
     }
 
+    /**
+     * This method returns the time the SMS was sent.
+     * @return the time the SMS was sent.
+     */
     public String getOraInvio() {
         return oraInvio;
     }
 
+    /**
+     * This method returns <code>true</code> if SMS is configured,
+     * <code>false</code> otherwise.
+     * @return <code>true</code> if SMS is configured, <code>false</code> otherwise.
+     */
     public boolean isConfigurato() {
         return configurato;
     }
 
+    /**
+     * This method sets whether the sms is configured or not.
+     */
     public void setConfigurato(boolean configurato) {
         this.configurato = configurato;
     }
 
+    /**
+     * This method sets up the SMS configuration.
+     * @param configurazioneSMS the SMS configuration.
+     */
     public void setMessaggioConfigurato(ConfigurazioneSMS configurazioneSMS){
+        Objects.requireNonNull(configurazioneSMS);
         this.setTesto(configurazioneSMS.getTestoConfigurato());
     }
 
