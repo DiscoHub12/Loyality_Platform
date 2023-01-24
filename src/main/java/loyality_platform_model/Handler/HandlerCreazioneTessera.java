8c3d44e22f4f0737aecf0a9ebfcebc1df9e241a8
IamFab01 <fabio.evangelista@studenti.unicam.it>
2023-01-24 15:09:02 +0100

    [UPDATE] Classes

--- src/main/java/loyality_platform_model/Handler/HandlerCreazioneTessera.java
+++ src/main/java/loyality_platform_model/Handler/HandlerCreazioneTessera.java
@@ -2,26 +2,31 @@ package loyality_platform_model.Handler;
 
 import loyality_platform_model.DBController.DBMSController;
 import loyality_platform_model.Models.Cliente;
-
+import loyality_platform_model.Models.Tessera;
 import java.util.Objects;
-
+/**
+ * IMPLEMENTED BY : Fabio Evangelista.
+ */
+/**
+ * Class that represents the possibility of creating a new customer card
+ * and sending it to the database
+ */
 public class HandlerCreazioneTessera {
-
     private final DBMSController dbmsController;
-
-
-
     public HandlerCreazioneTessera(){
-   this.dbmsController=DBMSController.getInstance();
-
+        this.dbmsController=DBMSController.getInstance();
     }
-
-    public boolean creaTessera(Cliente cliente){
-        Objects.requireNonNull(cliente);
-        //Todo implementare
-        return false;
-    }
-    public void inviaTessera(){
-        //TODO implementare
+    /**
+     * method that creates a new card for the customer,
+     * checks if the passed object is null or not.
+     * Based on the result, the card is created and passed to the Database.
+     * @param c customer for which to create the card
+     * @return true se l'oggetto passato non era nullo
+     */
+    public boolean creaTessera(Cliente c){
+        Objects.requireNonNull(c);
+        Tessera tessera= new Tessera(c);
+        this.dbmsController.addTessera(tessera,c);
+        return true;
     }
 }
--- src/main/java/loyality_platform_model/Handler/HandlerCreazioneUtente.java
+++ src/main/java/loyality_platform_model/Handler/HandlerCreazioneUtente.java
@@ -1,31 +1,48 @@
 package loyality_platform_model.Handler;
 
+import loyality_platform_model.Models.*;
 import loyality_platform_model.DBController.DBMSController;
-import loyality_platform_model.Models.Dipendente;
-import loyality_platform_model.Models.GestorePuntoVendita;
-
-
 import java.util.Objects;
-
+/**
+ * IMPLEMENTED BY : Fabio Evangelista.
+ */
+/**
+ * Class that represents the possibility of creating a new user
+ * and sending it to the database
+ */
 public class HandlerCreazioneUtente {
 
     private final DBMSController dbmsController;
     private Dipendente dipendente;
+    public final Azienda azienda;
 
-    public HandlerCreazioneUtente() {
+    public HandlerCreazioneUtente(Azienda azienda) {
         this.dbmsController = DBMSController.getInstance();
+        this.azienda= azienda;
+    }
+    public Azienda getAzienda() {
+        return azienda;
+    }
+    /**
+     * this method creates a user,
+     * check if the passed parameters are null or not.
+     * Based on the result it creates the employee,
+     * passes it to the Company class and to the Database
+     * @param nome employee name
+     * @param cognome employee surname
+     * @param email employee email
+     * @param restrizioni restrictions imposed on the employee
+     */
+    public void creaUtente(String nome, String cognome, String email, boolean restrizioni) {
+        if(Objects.equals(nome, ""))
+            throw new IllegalArgumentException("Illegal name for user");
+        if(Objects.equals(cognome, ""))
+            throw new IllegalArgumentException("Illegal surname for user");
+        if(Objects.equals(email, ""))
+            throw new IllegalArgumentException("Illegal email for user");
+        Dipendente dipendente= new  Dipendente(nome, cognome, email, restrizioni);
+        this.getAzienda().addDipendente(dipendente);
+        this.dbmsController.addDipendenteAzienda(this.getAzienda(), dipendente);
     }
 
-    public void creaDipendente(String nome, String cognome, String email) {
-        Objects.requireNonNull(nome);
-        Objects.requireNonNull(cognome);
-        Objects.requireNonNull(email);
-       // Dipendente dipendente = new Dipendente(nome, cognome, email);
-        //this.dbmsController.addDipendente(dipendente);
-        //da vedere
-    }
-    public void inviaUtente(Dipendente dipednente){
-        //TODO implementare.. ma a che serve?
-
-    }
 }
