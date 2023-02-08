27f0fdafc76c8ce038d04cd9c82954827afb6f51
Sofia <scattolinisofia26@gmail.com>
2023-02-08 18:37:26 +0100

    [IMPLEMENT] class Tipo for loyality program

--- src/main/java/loyality_platform_model/Models/Tipo.java
+++ src/main/java/loyality_platform_model/Models/Tipo.java
@@ -1,7 +1,10 @@
 package loyality_platform_model.Models;
 
+/**
+ * This class represents a Type that a given loyalty program can take on.
+ */
 public enum Tipo {
 
-    ProgrammaPunti, ProgrammaLivelli;
+    PROGRAMMAPUNTI, PROGRAMMALIVELLI, PROGRAMMACASHBACK;
 
 }
