1ec4eedaeb11fc7c02afe3a184b0becf268304cb
IamFab01 <fabio.evangelista@studenti.unicam.it>
2023-02-09 10:37:42 +0100

    [CHANGE] SMS Classes

--- src/main/java/loyality_platform_model/Models/PacchettoSMS.java
+++ src/main/java/loyality_platform_model/Models/PacchettoSMS.java
@@ -1,51 +1,63 @@
 package loyality_platform_model.Models;
 /**
- * IMPLEMENTED BY : Fabio Evangelista.
- */
-/**
+ /**
  * Class representing an SMS packet.
  * It contains an id, the price and the messages available inside
  */
 public class PacchettoSMS {
     /**
-     *attribute indicating the package id
+     * attribute indicating the package id
      */
-    private static int id;
+    private static int idPacchettoSMS;
     /**
-     *attribute indicating the price of the package
+     * attribute indicating the price of the package
      */
     private double prezzo;
     /**
-     *attribute indicating the messages available within a package
+     * attribute indicating the messages available within a package
      */
-    private int messaggiDisponibili;
+    private int numeroMessaggi;
+
     /**
-     *Constructor who creates an SMS package.
-     * @param prezzo the price of a package
-     * @param messaggiDisponibili Messages you can send are available
+     * Constructor who creates an SMS package.
+     *
+     * @param prezzo         the price of a package
+     * @param numeroMessaggi Messages you can send are available
      */
 
-    public PacchettoSMS(double prezzo,int messaggiDisponibili){
-        id++;
-        this.prezzo=prezzo;
-        this.messaggiDisponibili=messaggiDisponibili;
+    public PacchettoSMS(double prezzo, int numeroMessaggi) {
+        idPacchettoSMS++;
+        this.prezzo = prezzo;
+        this.numeroMessaggi = numeroMessaggi;
     }
 
-    public int getId(){
-        return id;
-    }
-    public double getPrezzo(){
-        return prezzo;
-    }
-    public void setPrezzo(double p){
-        this.prezzo=p;
-    }
-    public void setMessaggiDisponibili(int n){
-        this.messaggiDisponibili=n;
+    public int getId() {
+        return this.idPacchettoSMS;
     }
 
-    public int getMessaggiDisponibili() {
-        return messaggiDisponibili;
+    public double getPrezzo() {
+        return this.prezzo;
+    }
+
+    public void setPrezzo(double p) {
+        this.prezzo = p;
+    }
+
+    public void setNumeroMessaggi(int n) {
+        this.numeroMessaggi = n;
+    }
+
+    public int getNumeroMessaggi() {
+        return this.numeroMessaggi;
+    }
+
+
+    public String toString() {
+        return "Pacchetto SMS{" +
+                "id =" + this.idPacchettoSMS +
+                ", prezzo =" + this.prezzo +
+                ", numero dei messaggi =" + this.numeroMessaggi +
+                '}';
     }
 }
 
