f7cc120545a3e3dec19346f29f59047e161aadef
DiscoHub12 <discordo12di@gmail.com>
2023-02-09 11:19:09 +0100

    [ADD] All method for HandlerAzienda.java class.

--- src/main/java/loyality_platform_model/Handler/HandlerAzienda.java
+++ src/main/java/loyality_platform_model/Handler/HandlerAzienda.java
@@ -1,12 +1,14 @@
 package loyality_platform_model.Handler;
 
 import loyality_platform_model.DBMS.DBMS;
-import loyality_platform_model.Models.Azienda;
+import loyality_platform_model.Models.*;
+
 import java.util.Objects;
+import java.util.Set;
 
 /**
- *  Classes that represent capable a company manager
- *  to retrieve all available information.
+ * Classes that represent capable a company manager
+ * to retrieve all available information.
  */
 public class HandlerAzienda {
 
@@ -18,5 +20,53 @@ public class HandlerAzienda {
         this.dbms = DBMS.getInstance();
     }
 
-    //Todo implementare.
+    public GestorePuntoVendita getTitolareAzienda(int idAzienda) {
+        //Todo implementare
+        return null;
+    }
+
+    public Set<Dipendente> getDipendentiAzienda(int idAzienda) {
+        //Todo implementare
+        return null;
+    }
+
+    public void creaDipendente(int idAzienda, String nome, String cognome, String email) {
+        //Todo implementare
+    }
+
+    public void aggiungiDipendente(int idAzienda, String nome, String cognome, String email) {
+        //Todo implementare
+    }
+
+    public void modificaDipendente(int idAzienda, String nome, String cognome, String email) {
+        //Todo implementare
+    }
+
+    public void rimuoviDipendente(int idAzienda, Dipendente dipendente) {
+        //Todo implementare
+    }
+
+    public SpazioFedelta getSpazioFedeltaAzienda(int idAzienda) {
+        //Todo implementare
+        return null;
+    }
+
+    public void modificaSpazioFedelta(int idAzienda) {
+        //Todo implementare
+    }
+
+    public Set<ProgrammaFedelta> getProgrammiFedeltaAzienda(int idAzienda) {
+        //Todo implementare
+        return null;
+    }
+
+    public Set<CatalogoPremi> getCatalogoPremiAzienda(int idAzienda) {
+        //Todo implementare
+        return null;
+    }
+
+    public Set<Cliente> getClientiAzienda(int idAzienda, Coalizione coalizione) {
+        //Todo implementare
+        return null;
+    }
 }
