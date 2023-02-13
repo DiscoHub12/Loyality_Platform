943d750a5c8d93701b180abd0b5bdc8265302953
IamFab01 <fabio.evangelista@studenti.unicam.it>
2023-02-13 12:10:12 +0100

    [IMPLEMENT] method for HandlerMessaggi.java

--- src/main/java/loyality_platform_model/Handler/HandlerMessaggi.java
+++ src/main/java/loyality_platform_model/Handler/HandlerMessaggi.java
@@ -1,22 +1,20 @@
 package loyality_platform_model.Handler;
 
 import loyality_platform_model.DBMS.DBMS;
+import loyality_platform_model.Models.Azienda;
 import loyality_platform_model.Models.Cliente;
 import loyality_platform_model.Models.ConfigurazioneSMS;
 import loyality_platform_model.Models.SMS;
+
+import java.util.Date;
 import java.util.Objects;
 import java.util.Set;
 
 /**
- * IMPLEMENTED BY : Sofia Scattolini.
- *
  * Classes representing a capable message manager
  * to send text messages to a single customer or to a group of customers.
  */
 public class HandlerMessaggi {
-
-    //TODO rivedere
-    //TODO implementare db
 
     private final DBMS dbms;
 
@@ -27,31 +25,56 @@ public class HandlerMessaggi {
     public DBMS getDbmsController() {
         return dbms;
     }
+    public Set<SMS> getSMSPreconfigurati(int idAzienda){
+        if (idAzienda<=0)
+            throw new IllegalArgumentException("Illegal id for the company.");
+        for (Azienda azienda : this.dbms.getSMSPreconfiguratiAzienda().keySet()) {
+            if (azienda.getIdAzienda() == idAzienda) {
+                if (!this.dbms.getSMSCliente().get(azienda).isEmpty())
+                    return this.dbms.getSMSPreconfiguratiAzienda().get(azienda);
+            }
+        }
+        return null;
+    }
+
+    public ConfigurazioneSMS getSMSPreconfigurato(int idAzienda,int idSMS){
+        if (idAzienda<=0 || idSMS<=0)
+            throw new IllegalArgumentException("Illegal id for the company and sms.");
+        for (Azienda azienda : this.dbms.getAziendeIscritte()) {
+            if (azienda.getIdAzienda() == idAzienda) {
+                for(SMS sms : this.dbms.getSMSPreconfiguratiAzienda().get(azienda)){
+                    if(sms.getIdSMS()==idSMS){
+                        return sms.getConfigurazione();
+                    }
+                }
+            }
+        }
+        return null;
+    }
 
     /**
      * This method allows you to create a new SMS.
      * @param testo sms text.
-     * @param oraInvio sms ora.
      * @return new sms.
      */
-    public SMS creaSMS(String testo, String oraInvio){
+    public SMS creaSMS(String testo){
         if (Objects.equals(testo, ""))
             throw new IllegalArgumentException("Illegal text for sms.");
-        if (Objects.equals(oraInvio, ""))
-            throw new IllegalArgumentException("Illegal ora for sms.");
-       return new SMS(testo, oraInvio);
+        return new SMS(testo);
+    }
+    public void creaConfigurazioneSMS(String testo){
+        if (Objects.equals(testo, ""))
+            throw new IllegalArgumentException("Illegal text for sms.");
+        new ConfigurazioneSMS(testo);
     }
 
     /**
      * This method allows you to send a text message to a customer.
      * @param sms sms to send.
-     * @param cliente customer who must receive the SMS.
      */
-    public void inviaSMS(SMS sms, Cliente cliente){
-        Objects.requireNonNull(sms);
-        Objects.requireNonNull(cliente);
-        SMS sms1 = creaSMS(sms.getTesto(), sms.getOraInvio());
-        HandlerCliente.getInstance(cliente).getSMSCliente().add(sms1);
+
+    public void inviaSMS(int idCliente, String sms) {
+        //TODO da progettare
     }
 
     /**
@@ -59,46 +82,24 @@ public class HandlerMessaggi {
      * @param sms sms to send.
      * @param clienti set of customers who must receive the SMS.
      */
+
     public void inviaSmsGenerale(SMS sms, Set<Cliente> clienti){
-        Objects.requireNonNull(sms);
-        Objects.requireNonNull(clienti);
-        SMS sms1 = creaSMS(sms.getTesto(), sms.getOraInvio());
-        for(Cliente cliente : clienti){
-            HandlerCliente.getInstance(cliente).getSMSCliente().add(sms1);
-        }
+        //TODO implementare
     }
 
     /**
      * This method allows you to send a configured text message to a customer.
-     * @param sms sms to send.
-     * @param configurazioneSMS configured text message.
-     * @param cliente customer who must receive the SMS.
      */
-    public void inviaSMSConfigurato(SMS sms, ConfigurazioneSMS configurazioneSMS, Cliente cliente){
-        Objects.requireNonNull(sms);
-        Objects.requireNonNull(configurazioneSMS);
-        Objects.requireNonNull(cliente);
-        sms.setConfigurato(true);
-        sms.setMessaggioConfigurato(configurazioneSMS);
-        SMS sms1 = creaSMS(sms.getTesto(), sms.getOraInvio());
-        HandlerCliente.getInstance(cliente).getSMSCliente().add(sms1);
+
+    public void inviaSMSPreConfigurato(){
+        //TODO implementare
     }
 
     /**
      * This method allows you to send a configured text message to a set of customers.
-     * @param sms sms to send.
-     * @param configurazioneSMS configured text message.
-     * @param clienti set of customers who must receive the SMS.
      */
-    public void inviaSMSGeneraleConfigurato(SMS sms, ConfigurazioneSMS configurazioneSMS, Set<Cliente> clienti){
-        Objects.requireNonNull(sms);
-        Objects.requireNonNull(clienti);
-        Objects.requireNonNull(configurazioneSMS);
-        sms.setMessaggioConfigurato(configurazioneSMS);
-        sms.setConfigurato(true);
-        SMS sms1 = creaSMS(sms.getTesto(), sms.getOraInvio());
-        for(Cliente cliente : clienti){
-            HandlerCliente.getInstance(cliente).getSMSCliente().add(sms1);
-        }
+
+    public void inviaSMSPreconfiguratoGenerale(){
+        //TODO implementare
     }
 }
