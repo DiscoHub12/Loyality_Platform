1f46813117ec95f1e7e0e14ec56bd74da63361d6
IamFab01 <fabio.evangelista@studenti.unicam.it>
2023-02-22 12:21:08 +0100

    [BUGFIX] HandlerMessaggi method for DBMS

--- src/main/java/loyality_platform_model/DBMS/DBMS.java
+++ src/main/java/loyality_platform_model/DBMS/DBMS.java
@@ -654,7 +654,7 @@ public class DBMS {
     }
 
     public boolean addConfigurazioneSMS(int idAzienda, SMS smsPreconfigurato) {
-        for(Azienda azienda : this.getSMSPreconfiguratiAzienda().keySet()){
+        for(Azienda azienda : this.getAziendeIscritte()){
             if(azienda.getIdAzienda() == idAzienda){
                 if(this.getSMSPreconfiguratiAzienda().containsKey(azienda)){
                     if(this.getSMSPreconfiguratiAzienda().get(azienda) == null){
