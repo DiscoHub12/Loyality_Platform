710214d9918605e488dd3421629e4997c1e50231
DiscoHub12 <discordo12di@gmail.com>
2023-02-23 11:52:01 +0100

    [ADD] attributes on InitProjectData.java class and finish Main.

--- src/main/java/loyality_platform_model/Main/LoyalityPlatformApplication.java
+++ src/main/java/loyality_platform_model/Main/LoyalityPlatformApplication.java
@@ -1,4 +1,6 @@
 package loyality_platform_model.Main;
+
+import loyality_platform_model.Interface.UI_Cliente;
 import loyality_platform_model.Interface.UI_Home;
 import loyality_platform_model.Utils.InitProjectData;
 
@@ -6,7 +8,7 @@ import loyality_platform_model.Utils.InitProjectData;
 public class LoyalityPlatformApplication {
     public static void main(String[] args) {
 
-       InitProjectData.getInstance();
+        InitProjectData.getInstance();
         UI_Home home = new UI_Home();
         home.welcomePage();
 
